08a6af8cbbf78de902714b1da2d79d76572b891c
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2021-05-22 12:51:41 +0300

    getit init

---
+++ lib/locator.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+import 'package:remood/blocs/authBloc/bloc/auth_bloc.dart';
+import 'package:remood/data/databaseApiClient.dart';
+
+import 'data/authApiClient.dart';
+import 'data/authRepository.dart';
+import 'data/databaseRepository.dart';
+
+GetIt getIt = GetIt.instance;
+
+void setupLocator() {
+  getIt.registerLazySingleton<AuthRepository>(() => AuthRepository());
+  getIt.registerLazySingleton<AuthApiClient>(() => AuthApiClient());
+  getIt.registerLazySingleton<DatabaseRepository>(() => DatabaseRepository());
+  getIt.registerLazySingleton<DatabaseApiClient>(() => DatabaseApiClient());
+  getIt.registerLazySingleton<AuthBloc>(() => AuthBloc());
+}
